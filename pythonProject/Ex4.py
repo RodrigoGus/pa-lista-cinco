3f35eb2b1c1fff14aea847082b877d2935ac8278
RodrigoGus <rodrigogus1337@gmail.com>
2023-05-11 20:31:44 -0300

    ex 4 finish

--- pythonProject/Ex4.py
+++ pythonProject/Ex4.py
@@ -1,19 +1,19 @@
 import random
-
 a = []
-b = []
-a2 = []
-b2 = []
-
-for i in range(0, 10, 2):
-#    x = int(input(f'insira o {i + 1}° valor:    '))
-    x = int(random.randint(100))
+for r in range(10):
+    x = (random.randint(0,100))
     a.append(x)
-    for i2 in range(1, 10, 2):
-#        x = int(input(f'insira o {i + 1}° valor:    '))
-        x = int(random.randint(100))
-        a.append(x)
-
-y = 1
-a2.copy(a)
-b2.copy(b)
+print(a)
+for n in range(0, 10, 2):
+    y = a[n]
+    y *= 3
+    a.pop(n)
+    a.insert(n, y)
+    n += 2
+for g in range(1, 10, 2):
+    y = a[g]
+    y /= 2
+    a.pop(g)
+    a.insert(g, y)
+    g += 2
+print(a)
